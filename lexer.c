8a2376d215acb1855b1ae16d75bcbeca06e675e4
Swastik-Mantry <f20191019@pilani.bits-pilani.ac.in>
2023-03-01 23:45:28 +0530

    Add Group Comments and correct Multiple lexical prints

--- lexer.c
+++ lexer.c
@@ -1,3 +1,11 @@
+/*
+Group Number : 2
+1 	Dhruv Rawat 	2019B3A70537P 	thedhruvrawat
+2 	Chirag Gupta 	2019B3A70555P 	Chirag5128
+3 	Swastik Mantry 	2019B1A71019P 	Swastik-Mantry
+4 	Shreyas Sheeranali 	2019B3A70387P 	ShreyasSR
+5 	Vaibhav Prabhu 	2019B3A70593P 	prabhuvaibhav
+*/
 #include "lexer.h"
 
 // Buffers
@@ -39,7 +47,7 @@ static FILE* fp;
 static int count = 0;
 static int lastBufLoad = 0;
 static bool twice = false;
-
+static bool LexCorrPrint = true; // Printed Lexically Correct
 // checking for character in sync set
 bool inSyncSetA(char c){
     switch(c){
@@ -221,9 +229,10 @@ TOKEN* getNextToken(){
                 } else if (curr == '.') {
                     state = 43;
                 } else if(curr == EOF){
-                    if(errno==0){
+                    if(errno==0 && LexCorrPrint){
                         // Should we add this at all curr==EOF?
                         printf(GREEN BOLD "Input source code is lexically correct\n" RESET);
+                        LexCorrPrint = false;
                     }
                     strcpy(token->lexeme, "EOF");
                     token->linenum = LINE_NUM;
--- trie.h
+++ trie.h
@@ -1,3 +1,11 @@
+/*
+Group Number : 2
+1 	Dhruv Rawat 	2019B3A70537P 	thedhruvrawat
+2 	Chirag Gupta 	2019B3A70555P 	Chirag5128
+3 	Swastik Mantry 	2019B1A71019P 	Swastik-Mantry
+4 	Shreyas Sheeranali 	2019B3A70387P 	ShreyasSR
+5 	Vaibhav Prabhu 	2019B3A70593P 	prabhuvaibhav
+*/
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
