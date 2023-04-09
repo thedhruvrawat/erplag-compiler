156fecc671db480e6562912fe7bc205d201e5ce9
thedhruvrawat <dhruv.noida175@gmail.com>
2023-04-09 15:46:21 +0530

    Add basic codegen (untested)

---
+++ codegen.c
@@ -0,0 +1,500 @@
+/*
+Group Number : 2
+1 	Dhruv Rawat 	    2019B3A70537P 	thedhruvrawat
+2 	Chirag Gupta 	    2019B3A70555P 	Chirag5128
+3 	Swastik Mantry 	    2019B1A71019P 	Swastik-Mantry
+4 	Shreyas Sheeranali 	2019B3A70387P 	ShreyasSR
+5 	Vaibhav Prabhu 	    2019B3A70593P 	prabhuvaibhav
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "codegen.h"
+#include "symbolTable.h"
+
+int labelCount = 0;
+
+char *getNewLabelVariable() {
+    char *label = (char*)malloc(sizeof(char) * 10);
+    snprintf(label, 10, "__label_%d", labelCount);
+    labelCount++;
+    return label;
+}
+
+void codeGenerator(QuadrupleTable *qt, char *output) {
+    FILE *codefile = fopen(output, "w");
+
+    initASMFile(codefile);
+
+    Quadruple* currQuad = qt->head;
+
+    while(currQuad!=NULL) {
+        switch(currQuad->op) {
+            case PLUS_OP: {
+                if(currQuad->arg1Type == REAL && currQuad->arg2Type == REAL) {
+                    insertArithmeticOperation(codefile, currQuad, '+', 'F');
+                } else {
+                    insertArithmeticOperation(codefile, currQuad, '+', 'I');
+                }
+                break;
+            }
+            case MINUS_OP: {
+                if(currQuad->arg1Type == REAL && currQuad->arg2Type == REAL) {
+                    insertArithmeticOperation(codefile, currQuad, '-', 'F');
+                } else {
+                    insertArithmeticOperation(codefile, currQuad, '-', 'I');
+                }
+                break;
+            }
+            case MUL_OP: {
+                if(currQuad->arg1Type == REAL && currQuad->arg2Type == REAL) {
+                    insertArithmeticOperation(codefile, currQuad, '*', 'F');
+                } else {
+                    insertArithmeticOperation(codefile, currQuad, '*', 'I');
+                }
+                break;
+            }
+            case DIV_OP: {
+                insertArithmeticOperation(codefile, currQuad, '/', 'F');
+                break;
+            }
+            case LT_OP: {
+                if(currQuad->arg1Type == REAL && currQuad->arg2Type == REAL) {
+                    insertRelationalOperation(codefile, currQuad, '<', 'F');
+                } else {
+                    insertRelationalOperation(codefile, currQuad, '<', 'I');
+                }
+                break;
+            }
+            case LE_OP: {
+                if(currQuad->arg1Type == REAL && currQuad->arg2Type == REAL) {
+                    insertRelationalOperation(codefile, currQuad, 'L', 'F');
+                } else {
+                    insertRelationalOperation(codefile, currQuad, 'L', 'I');
+                }
+                break;
+            }
+            case GT_OP: {
+                if(currQuad->arg1Type == REAL && currQuad->arg2Type == REAL) {
+                    insertRelationalOperation(codefile, currQuad, '>', 'F');
+                } else {
+                    insertRelationalOperation(codefile, currQuad, '>', 'I');
+                }
+                break;
+            }
+            case GE_OP: {
+                if(currQuad->arg1Type == REAL && currQuad->arg2Type == REAL) {
+                    insertRelationalOperation(codefile, currQuad, 'G', 'F');
+                } else {
+                    insertRelationalOperation(codefile, currQuad, 'G', 'I');
+                }
+                break;
+            }
+            case EQ_OP: {
+                if(currQuad->arg1Type == REAL && currQuad->arg2Type == REAL) {
+                    insertRelationalOperation(codefile, currQuad, '=', 'F');
+                } else if(currQuad->arg1Type == INTEGER && currQuad->arg2Type == INTEGER){
+                    insertRelationalOperation(codefile, currQuad, '=', 'I');
+                } else {
+                    insertRelationalOperation(codefile, currQuad, '=', 'B');
+                }
+                break;
+            }
+            case NE_OP: {
+                if(currQuad->arg1Type == REAL && currQuad->arg2Type == REAL) {
+                    insertRelationalOperation(codefile, currQuad, 'N', 'F');
+                } else if(currQuad->arg1Type == INTEGER && currQuad->arg2Type == INTEGER){
+                    insertRelationalOperation(codefile, currQuad, 'N', 'I');
+                } else {
+                    insertRelationalOperation(codefile, currQuad, 'N', 'B');
+                }
+                break;
+            }
+            case AND_OP: {
+                insertLogicalOperation(codefile, currQuad, '&');
+                break;
+            }
+            case OR_OP: {
+                insertLogicalOperation(codefile, currQuad, '|');
+                break;
+            }
+            case UMINUS_OP: {
+                if(currQuad->arg1Type == REAL && currQuad->arg2Type == REAL) {
+                    insertUnaryMinusOperation(codefile, currQuad, 'F');
+                } else {
+                    insertUnaryMinusOperation(codefile, currQuad, 'I');
+                }
+                break;
+            }
+            case ASSIGN_QOP: {
+                if(currQuad->arg1Type == REAL && currQuad->arg2Type == REAL) {
+                    insertAssignmentOperation(codefile, currQuad, 'F');
+                } else if(currQuad->arg1Type == INTEGER && currQuad->arg2Type == INTEGER){
+                    insertAssignmentOperation(codefile, currQuad, 'I');
+                } else {
+                    insertAssignmentOperation(codefile, currQuad, 'B');
+                }
+                break;
+            }
+        }
+        currQuad = currQuad->next;
+    }
+
+    fprintf(codefile, "\tpop rbp\n");
+    fprintf(codefile, "\tmov rax, 0\n");
+    fprintf(codefile, "\tret\n");
+    fclose(codefile);
+}
+
+void initASMFile(FILE *codefile) {
+    fprintf(codefile, "extern printf\n");
+    fprintf(codefile, "section .text\n");
+    fprintf(codefile, "\tglobal main\n");
+    fprintf(codefile, "main:\n");
+    fprintf(codefile, "\tpush rbp\n");
+}
+
+void insertArithmeticOperation(FILE *codefile, Quadruple *q, char op, char type) {
+    int arg1Offset = -1, arg2Offset = -1, resultOffset = -1;
+    if(q->isArg1ID) { //Is a Record
+        arg1Offset = q->arg1ID->offset;
+    }
+    if(q->isArg2ID) { //Is a Record
+        arg2Offset = q->arg2ID->offset;
+    }
+    resultOffset = q->result->offset;
+    if(type == 'I') {
+        if(q->isArg1ID) {
+            fprintf(codefile, "\tMOVSXD rax, DWORD[rbp-8-%d]\n", arg1Offset*8);
+        } else {
+            fprintf(codefile, "\tMOV rax, %s\n", q->arg1Num);
+        }
+        if(q->isArg2ID) {
+            fprintf(codefile, "\tMOVSXD rbx, DWORD[rbp-8-%d]\n", arg2Offset*8);
+        } else {
+            fprintf(codefile, "\tMOV rbx, %s\n", q->arg2Num);
+        }
+        switch(op) {
+            case '+': {
+                fprintf(codefile, "\t; ADDITION\n");
+                fprintf(codefile, "\tADD rax, rbx\n");
+                break;
+            }
+            case '-': {
+                fprintf(codefile, "\t; SUBTRACTION\n");
+                fprintf(codefile, "\tSUB rax, rbx\n");
+                break;
+            }
+            case '*': {
+                fprintf(codefile, "\t; MULTIPLICATION\n");
+                fprintf(codefile, "\tIMUL rax, rbx\n");
+                break;
+            }
+            case '/': {
+                fprintf(codefile, "\t; DIVISION\n");
+                fprintf(codefile, "\tIDIV rax, rbx\n");
+                break;
+            }
+        }
+        fprintf(codefile, "\tMOV QWORD[rbp-8-%d], rax\n", resultOffset*8);
+    } else { //Floating point arithmetic
+        if(q->isArg1ID) {
+            fprintf(codefile, "\tMOVSD xmm0, QWORD[rbp-8-%d]\n", arg1Offset*16);            
+        } else {
+            char *arg1Label = getNewLabelVariable();
+            fprintf(codefile,"\tsection .data\n");
+            fprintf(codefile,"\t%s: dq %s\n", arg1Label, q->arg1Real);
+            fprintf(codefile,"\tsection .text\n");
+            fprintf(codefile,"\tMOVSD xmm0, [%s]\n", arg1Label);
+        }
+        if(q->isArg2ID) {
+            fprintf(codefile, "\tMOVSD xmm1, QWORD[rbp-8-%d]\n", arg2Offset*16);            
+        } else {
+            char *arg2Label = getNewLabelVariable();
+            fprintf(codefile,"\tsection .data\n");
+            fprintf(codefile,"\t%s: dq %s\n", arg2Label, q->arg2Real);
+            fprintf(codefile,"\tsection .text\n");
+            fprintf(codefile,"\tMOVSD xmm1, [%s]\n", arg2Label);
+        }
+        switch(op) {
+            case '+': {
+                fprintf(codefile, "\t; ADDITION\n");
+                fprintf(codefile, "\tADDSD xmm0, xmm1\n");
+                break;
+            }
+            case '-': {
+                fprintf(codefile, "\t; SUBTRACTION\n");
+                fprintf(codefile, "\tSUBSD xmm0, xmm1\n");
+                break;
+            }
+            case '*': {
+                fprintf(codefile, "\t; MULTIPLICATION\n");
+                fprintf(codefile, "\tMULSD xmm0, xmm1\n");
+                break;
+            }
+            case '/': {
+                fprintf(codefile, "\t; DIVISION\n");
+                fprintf(codefile, "\tDIVSD xmm0, xmm1\n");
+                break;
+            }
+        }
+        fprintf(codefile, "\tMOV QWORD[rbp-8-%d], xmm0\n", resultOffset*16);        
+    }    
+}
+
+
+void insertRelationalOperation(FILE *codefile, Quadruple *q, char op, char type) {
+    int arg1Offset = -1, arg2Offset = -1, resultOffset = -1;
+    if(q->isArg1ID) { //Is a Record
+        arg1Offset = q->arg1ID->offset;
+    }
+    if(q->isArg2ID) { //Is a Record
+        arg2Offset = q->arg2ID->offset;
+    }
+    resultOffset = q->result->offset;
+    fprintf(codefile, "\tMOV rcx, 0\n"); //False
+    fprintf(codefile, "\tMOV rdx, 1\n"); //True
+    if(type == 'I') {
+        if(q->isArg1ID) {
+            fprintf(codefile, "\tMOVSXD rax, DWORD[rbp-8-%d]\n", arg1Offset*8);
+        } else {
+            fprintf(codefile, "\tMOV rax, %s\n", q->arg1Num);
+        }
+        if(q->isArg2ID) {
+            fprintf(codefile, "\tMOVSXD rbx, DWORD[rbp-8-%d]\n", arg2Offset*8);
+        } else {
+            fprintf(codefile, "\tMOV rbx, %s\n", q->arg2Num);
+        }
+        switch(op) {
+            case '<': {
+                fprintf(codefile, "\t; LESS THAN\n");
+                fprintf(codefile, "\tCMP rax, rbx\n");
+                fprintf(codefile, "\tCMOVL rcx, rdx\n");
+                break;
+            }
+            case 'L': {
+                fprintf(codefile, "\t; LESS THAN OR EQUAL TO\n");
+                fprintf(codefile, "\tCMP rax, rbx\n");
+                fprintf(codefile, "\tCMOVLE rcx, rdx\n");
+                break;
+            }
+            case '>': {
+                fprintf(codefile, "\t; GREATER THAN\n");
+                fprintf(codefile, "\tCMP rax, rbx\n");
+                fprintf(codefile, "\tCMOVG rcx, rdx\n");
+                break;
+            }
+            case 'G': {
+                fprintf(codefile, "\t; GREATER THAN OR EQUAL TO\n");
+                fprintf(codefile, "\tCMP rax, rbx\n");
+                fprintf(codefile, "\tCMOVGE rcx, rdx\n");
+                break;
+            }
+            case '=': {
+                fprintf(codefile, "\t; EQUAL TO\n");
+                fprintf(codefile, "\tCMP rax, rbx\n");
+                fprintf(codefile, "\tCMOVE rcx, rdx\n");
+                break;
+            }
+            case 'N': {
+                fprintf(codefile, "\t; NOT EQUAL TO\n");
+                fprintf(codefile, "\tCMP rax, rbx\n");
+                fprintf(codefile, "\tCMOVNE rcx, rdx\n");
+                break;
+            }
+        }
+        fprintf(codefile, "\tMOV DWORD[rbp-8-%d], rcx\n", resultOffset*8);
+    } else if (type == 'F') { //Floating point arithmetic
+        if(q->isArg1ID) {
+            fprintf(codefile, "\tMOVSD xmm0, QWORD[rbp-8-%d]\n", arg1Offset*16);            
+        } else {
+            char *arg1Label = getNewLabelVariable();
+            fprintf(codefile,"\tsection .data\n");
+            fprintf(codefile,"\t%s: dq %s\n", arg1Label, q->arg1Real);
+            fprintf(codefile,"\tsection .text\n");
+            fprintf(codefile,"\tMOVSD xmm0, [%s]\n", arg1Label);
+        }
+        if(q->isArg2ID) {
+            fprintf(codefile, "\tMOVSD xmm1, QWORD[rbp-8-%d]\n", arg2Offset*16);            
+        } else {
+            char *arg2Label = getNewLabelVariable();
+            fprintf(codefile,"\tsection .data\n");
+            fprintf(codefile,"\t%s: dq %s\n", arg2Label, q->arg2Real);
+            fprintf(codefile,"\tsection .text\n");
+            fprintf(codefile,"\tMOVSD xmm1, [%s]\n", arg2Label);
+        }
+        switch(op) {
+            case '<': {
+                fprintf(codefile, "\t; LESS THAN\n");
+                fprintf(codefile, "\tCMP xmm0, xmm1\n");
+                fprintf(codefile, "\tCMOVL rcx, rdx\n");
+                break;
+            }
+            case 'L': {
+                fprintf(codefile, "\t; LESS THAN OR EQUAL TO\n");
+                fprintf(codefile, "\tCMP xmm0, xmm1\n");
+                fprintf(codefile, "\tCMOVLE rcx, rdx\n");
+                break;
+            }
+            case '>': {
+                fprintf(codefile, "\t; GREATER THAN\n");
+                fprintf(codefile, "\tCMP xmm0, xmm1\n");
+                fprintf(codefile, "\tCMOVG rcx, rdx\n");
+                break;
+            }
+            case 'G': {
+                fprintf(codefile, "\t; GREATER THAN OR EQUAL TO\n");
+                fprintf(codefile, "\tCMP xmm0, xmm1\n");
+                fprintf(codefile, "\tCMOVGE rcx, rdx\n");
+                break;
+            }
+            case '=': {
+                fprintf(codefile, "\t; EQUAL TO\n");
+                fprintf(codefile, "\tCMP xmm0, xmm1\n");
+                fprintf(codefile, "\tCMOVE rcx, rdx\n");
+                break;
+            }
+            case 'N': {
+                fprintf(codefile, "\t; NOT EQUAL TO\n");
+                fprintf(codefile, "\tCMP xmm0, xmm1\n");
+                fprintf(codefile, "\tCMOVNE rcx, rdx\n");
+                break;
+            }
+        }
+        fprintf(codefile, "\tMOV QWORD[rbp-8-%d], rcx\n", resultOffset*16);        
+    } else { // Boolean
+        if(q->isArg1ID) {
+            fprintf(codefile, "\tMOVSXD rax, DWORD[rbp-8-%d]\n", arg1Offset*8);
+        } else {
+            fprintf(codefile, "\tMOV rax, %s\n", q->arg1Num);
+        }
+        if(q->isArg2ID) {
+            fprintf(codefile, "\tMOVSXD rbx, DWORD[rbp-8-%d]\n", arg2Offset*8);
+        } else {
+            fprintf(codefile, "\tMOV rbx, %s\n", q->arg2Num);
+        }
+        switch(op) {
+            case '=': {
+                fprintf(codefile, "\t; EQUAL TO\n");
+                fprintf(codefile, "\tCMP rax, rbx\n");
+                fprintf(codefile, "\tCMOVE rcx, rdx\n");
+                break;
+            }
+            case 'N': {
+                fprintf(codefile, "\t; NOT EQUAL TO\n");
+                fprintf(codefile, "\tCMP rax, rbx\n");
+                fprintf(codefile, "\tCMOVNE rcx, rdx\n");
+                break;
+            }
+        }
+        fprintf(codefile, "\tMOV DWORD[rbp-8-%d], rcx\n", resultOffset*8);
+    }
+}
+
+void insertLogicalOperation(FILE *codefile, Quadruple *q, char op) { //only Boolean operations
+    int arg1Offset = -1, arg2Offset = -1, resultOffset = -1;
+    if(q->isArg1ID) { //Is a Record
+        arg1Offset = q->arg1ID->offset;
+    }
+    if(q->isArg2ID) { //Is a Record
+        arg2Offset = q->arg2ID->offset;
+    }
+    resultOffset = q->result->offset;
+    fprintf(codefile, "\tMOV rcx, 0\n"); //False
+    fprintf(codefile, "\tMOV rdx, 1\n"); //True
+    if(q->isArg1ID) {
+        fprintf(codefile, "\tMOVSXD rax, DWORD[rbp-8-%d]\n", arg1Offset*8);
+    } else {
+        fprintf(codefile, "\tMOV rax, %s\n", q->arg1Bool);
+    }
+    if(q->isArg2ID) {
+        fprintf(codefile, "\tMOVSXD rbx, DWORD[rbp-8-%d]\n", arg2Offset*8);
+    } else {
+        fprintf(codefile, "\tMOV rbx, %s\n", q->arg2Bool);
+    }
+    switch(op) {
+        case '&': {
+            fprintf(codefile, "\t; LOGICAL AND\n");
+            fprintf(codefile, "\tADD rax, rbx\n");
+            fprintf(codefile, "\tCMP rax, 2\n");
+            fprintf(codefile, "\tCMOVE rcx, rdx\n");
+            break;
+        }
+        case '|': {
+            fprintf(codefile, "\t; LOGICAL OR\n");
+            fprintf(codefile, "\tSUB rax, rbx\n");
+            fprintf(codefile, "\tCMP rax, 0\n");
+            fprintf(codefile, "\tCMOVNE rcx, rdx\n");
+            break;
+        }
+    }
+    fprintf(codefile, "\tMOV DWORD[rbp-8-%d], rcx\n", resultOffset*8);
+}
+
+void insertUnaryMinusOperation(FILE *codefile, Quadruple *q, char type) { //only Boolean operations
+    int arg1Offset = -1, resultOffset = -1;
+    if(q->isArg1ID) { //Is a Record
+        arg1Offset = q->arg1ID->offset;
+    }
+    resultOffset = q->result->offset;
+    if(type == 'I') {
+        if(q->isArg1ID) {
+            fprintf(codefile, "\tMOVSXD rax, DWORD[rbp-8-%d]\n", arg1Offset*8);
+        } else {
+            fprintf(codefile, "\tMOV rax, %s\n", q->arg1Num);
+        }
+        fprintf(codefile, "\t; UNARY MINUS\n");
+        fprintf(codefile, "\tNEG rax\n");
+        fprintf(codefile, "\tMOV QWORD[rbp-8-%d], rax\n", resultOffset*8);
+    } else { //Floating point arithmetic
+        if(q->isArg1ID) {
+            fprintf(codefile, "\tMOVSD xmm0, QWORD[rbp-8-%d]\n", arg1Offset*16);            
+        } else {
+            char *arg1Label = getNewLabelVariable();
+            fprintf(codefile,"\tsection .data\n");
+            fprintf(codefile,"\t%s: dq %s\n", arg1Label, q->arg1Real);
+            fprintf(codefile,"\tsection .text\n");
+            fprintf(codefile,"\tMOVSD xmm0, [%s]\n", arg1Label);
+        }
+        fprintf(codefile, "\t; UNARY MINUS\n");
+        fprintf(codefile, "\tNEG xmm0\n");
+        fprintf(codefile, "\tMOV QWORD[rbp-8-%d], xmm0\n", resultOffset*16);        
+    }    
+}
+
+void insertAssignmentOperation(FILE *codefile, Quadruple *q, char type) { //only Boolean operations
+    int arg1Offset = -1, resultOffset = -1;
+    if(q->isArg1ID) { //Is a Record
+        arg1Offset = q->arg1ID->offset;
+    }
+    resultOffset = q->result->offset;
+
+    fprintf(codefile, "\t; ASSIGNMENT OPERATION (NON-ARRAY)\n");
+    if(type == 'I') {
+        if(q->isArg1ID) {
+            fprintf(codefile, "\tMOVSXD rax, DWORD[rbp-8-%d]\n", arg1Offset*8);
+        } else {
+            fprintf(codefile, "\tMOV rax, %s\n", q->arg1Num);
+        }
+        fprintf(codefile, "\tMOV QWORD[rbp-8-%d], rax\n", resultOffset*8);
+    } else if(type == 'F'){ //Floating point 
+        if(q->isArg1ID) {
+            fprintf(codefile, "\tMOVSD xmm0, QWORD[rbp-8-%d]\n", arg1Offset*16);            
+        } else {
+            char *arg1Label = getNewLabelVariable();
+            fprintf(codefile,"\tsection .data\n");
+            fprintf(codefile,"\t%s: dq %s\n", arg1Label, q->arg1Real);
+            fprintf(codefile,"\tsection .text\n");
+            fprintf(codefile,"\tMOVSD xmm0, [%s]\n", arg1Label);
+        }
+        fprintf(codefile, "\tMOV QWORD[rbp-8-%d], xmm0\n", resultOffset*16);        
+    } else { //Boolean
+        if(q->isArg1ID) {
+            fprintf(codefile, "\tMOVSXD rax, DWORD[rbp-8-%d]\n", arg1Offset*8);
+        } else {
+            fprintf(codefile, "\tMOV rax, %s\n", q->arg1Bool);
+        }
+        fprintf(codefile, "\tMOV DWORD[rbp-8-%d], rax\n", resultOffset*8);
+    }
+}
---
+++ codegen.h
@@ -0,0 +1,23 @@
+/*
+Group Number : 2
+1 	Dhruv Rawat 	    2019B3A70537P 	thedhruvrawat
+2 	Chirag Gupta 	    2019B3A70555P 	Chirag5128
+3 	Swastik Mantry 	    2019B1A71019P 	Swastik-Mantry
+4 	Shreyas Sheeranali 	2019B3A70387P 	ShreyasSR
+5 	Vaibhav Prabhu 	    2019B3A70593P 	prabhuvaibhav
+*/
+#ifndef CODEGEN_H
+#define CODEGEN_H
+
+#include "symbolTableDef.h"
+
+char *getNewLabelVariable();
+void codeGenerator(QuadrupleTable *qt, char *output);
+void initASMFile(FILE *fp);
+void insertArithmeticOperation(FILE *codefile, Quadruple *q, char op, char type);
+void insertRelationalOperation(FILE *codefile, Quadruple *q, char op, char type);
+void insertLogicalOperation(FILE *codefile, Quadruple *q, char op);
+void insertUnaryMinusOperation(FILE *codefile, Quadruple *q, char type);
+void insertAssignmentOperation(FILE *codefile, Quadruple *q, char type);
+
+#endif
